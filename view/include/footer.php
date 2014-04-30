56c2fc41d60b38cf36fd0d49404e4cfe3c44543f
peng.shen <peng.shen@indochino.com>
2014-04-29 17:00:27 -0700

    add favicon.ico

--- view/include/footer.php
+++ view/include/footer.php
@@ -1,5 +1,8 @@
 </div>
 <div id="footer">
+<div id="footer_inner">
+<div id="copy_right"><label>Copyright &copy; <?=date('Y') ?> Confone Inc. All Rights Reserved.</label></div>
+</div>
 </div>
 <?php if (isset($scripts)) {
     foreach ($scripts as $script) {
--- view/include/header.php
+++ view/include/header.php
@@ -1,12 +1,12 @@
-<?php
-global $base_host;
+<?php 
+global $base_host; 
 ?><!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?=(isset($title) ? $title : "Confone") ?></title>
+<title><?=isset($title) ? $title : '' ?></title>
 <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 <script src="/js/common.js"></script>
@@ -20,5 +20,8 @@ global $base_host;
 </head>
 <body>
 <div id="header">
+<div id="header_inner">
+<div id="logo" onclick="javascript:loadUrl('<?php global $www_url; echo $www_url; ?>');">con<span>fone</span></div>
+</div>
 </div>
 <div id="main">
