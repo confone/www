cad481d827559e287a658b8d9eda2d3cf8fda52f
pengshen001 <peng.shen@indochino.com>
2014-05-19 22:39:19 -0700

    update

--- view/include/header.php
+++ view/include/header.php
@@ -1,5 +1,5 @@
 <?php 
-global $base_host; 
+global $base_host, $account_url, $doc_url, $help_url;
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -25,10 +25,10 @@ global $base_host;
 <div id="header_inner">
 <div id="profile"></div>
 <div id="header_link">
-<a href="http://local.doc.confone.com/">Documentation</a>
-<a href="http://local.doc.confone.com/">Help &amp; Support</a>
+<a href="<?=$doc_url ?>">Documentation</a>
+<a href="<?=$help_url ?>">Help &amp; Support</a>
 </div>
-<a id="logo" href="<?php global $www_url; echo $www_url; ?>">con<span>fone</span></a>
+<a id="logo" href="<?=$base_host; ?>">con<span>fone</span></a>
 </div>
 </div>
 <div id="main">
